5fe637f08f7c0ae63d84637485835b468b19bc7f
krisszcode <Krissz970519>
2020-07-06 20:09:01 +0200

    maven project

---
+++ demo/src/main/java/com/example/demo/Bike.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Bike implements Vehicle {
+    public void drive(){
+        System.out.println("bikening");
+    }
+}
---
+++ demo/src/main/java/com/example/demo/Car.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Car implements Vehicle{
+    public void drive(){
+        System.out.println("tutuuuu drájvening ö kár");
+    }
+}
